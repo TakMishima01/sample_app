bf9c9b04098f93ccd0befd01d6d157997b4ddbd9
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2023-04-22 07:05:58 +0900

    [add]Rspec入力中

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -7,3 +7,41 @@ describe 'モデルのテスト' do
     expect(FactoryBot.build(:list)).to be_vaild
   end
 end
+describe '投稿のテスト' do
+  let!(:list) { create(:list,title:'hoge', body:'body')}
+  describe 'トップ画面(top_path)のテスト' do
+    before do
+      visit top_path
+    end
+    context '表示の確認' do
+      it 'トップ画面に「ここはTopページです」が表示されているか' do
+        expect(page).to have_content 'ここはTopページです'
+      end
+      it 'top_pathが"/top"であるか' do
+        expect(current_path).to eq('/top')
+      end
+    end
+  end
+  describe '投稿画面のテスト' do
+    befor do
+      visit new_list_path
+    end
+    context '表示の確認' do
+      it 'new_list_pathが"/list/new"であるか' do
+        expect(current_path).to eq('/list/new')
+      end
+      it '投稿ボタンが表示されているか' do
+        expect(page).to have_button '投稿'
+      end
+    end
+    context '投稿処理のテスト' do
+      it '投稿後のリダイレクト先は正しいか' do
+        fill_in 'list[title]', with: Faker::Lorem.characters(number:10)
+        fill_in 'list[body]', with: Faker::Lorem.characters(number:30)
+        click_button '投稿'
+        expect(page).to have_current_path list_path(List.last)
+      end
+    end
+  end
+  
+end
